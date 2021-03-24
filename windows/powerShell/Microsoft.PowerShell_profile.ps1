aec235b16fe86ecf6fcad13b50ff9f0f52e88be6
ken <ds_bing-hua.lin@mediatek.com>
2021-03-24 09:14:17 +0800

    modify config

--- windows/powerShell/Microsoft.PowerShell_profile.ps1
+++ windows/powerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 Import-Module posh-git
 Import-Module oh-my-posh
-Set-PoshPrompt -Theme ken # 要用的 Theme
+Set-PoshPrompt -Theme ken # 要用的 Theme ken = 主題名稱
 Set-Item -Path Env:Username -Value ($Env:Username = "ken") # your pc username，用來隱藏本機名
 Set-Item -Path Env:computername -Value ($Env:computername = "🚀 MTK 🚀") # your pc username，用來隱藏本機名
